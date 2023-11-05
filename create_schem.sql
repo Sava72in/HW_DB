4856ee0196c866b574dc1410909cf4e0488b93ae
Vyacheslav Chernyak <Vachernyak@mts.ru>
2023-11-05 15:26:46 +0300

    Fix create_schem

---
+++ create_schem.sql
@@ -0,0 +1,38 @@
+create table track(
+track_id serial primary key,
+name varchar(40) not null,
+duration time not null,
+album_id int references album(album_id))
+
+create table collection(
+collection_id serial primary key,
+name varchar(40) not null,
+year_of_issue date not null)
+
+create table collection_track(
+track_id int ,
+collection_id int,
+foreign key (track_id) references track(track_id),  
+foreign key (collection_id) references collection(collection_id))
+
+create table musical_genre(
+genre_id serial primary key,
+name varchar(40) not null)
+
+create table artist(
+artist_id serial primary key,
+name varchar(40) not null)
+
+create table artist_genre(
+artist_id int,
+genre_id int,
+foreign key (artist_id) references artist(artist_id),
+foreign key (genre_id) references musical_genre(genre_id))
+
+create table artist_album(
+album_id int,
+artist_id int,
+foreign key (artist_id) references artist(artist_id),
+foreign key (album_id) references album(album_id))
+
+
