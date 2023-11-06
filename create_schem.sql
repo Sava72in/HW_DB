da363ed3a6aa366c6bf22a2823535415494a0573
Vyacheslav Chernyak <Vachernyak@mts.ru>
2023-11-06 11:13:50 +0300

    Fixed errors

--- create_schem.sql
+++ create_schem.sql
@@ -1,38 +1,43 @@
-create table track(
-track_id serial primary key,
-name varchar(40) not null,
-duration time not null,
-album_id int references album(album_id))
-
-create table collection(
-collection_id serial primary key,
-name varchar(40) not null,
-year_of_issue date not null)
-
-create table collection_track(
-track_id int ,
-collection_id int,
-foreign key (track_id) references track(track_id),  
-foreign key (collection_id) references collection(collection_id))
-
-create table musical_genre(
+create table if not exists musical_genre(
 genre_id serial primary key,
-name varchar(40) not null)
+name varchar(40) not null);
 
-create table artist(
+create table if not exists artist(
 artist_id serial primary key,
-name varchar(40) not null)
+name varchar(40) not null);
 
-create table artist_genre(
+create table if not exists artist_genre(
 artist_id int,
 genre_id int,
 foreign key (artist_id) references artist(artist_id),
-foreign key (genre_id) references musical_genre(genre_id))
+foreign key (genre_id) references musical_genre(genre_id));
 
-create table artist_album(
+create table if not exists album(
+album_id serial primary key,
+name varchar(40) not null,
+year_of_issue date not null);
+
+create table if not exists artist_album(
 album_id int,
 artist_id int,
 foreign key (artist_id) references artist(artist_id),
-foreign key (album_id) references album(album_id))
+foreign key (album_id) references album(album_id));
+
+create table if not exists collection(
+collection_id serial primary key,
+name varchar(40) not null,
+year_of_issue date not null);
+
+create table if not exists track(
+track_id serial primary key,
+name varchar(40) not null,
+duration time not null,
+album_id int references album(album_id));
+
+create table if not exists collection_track(
+track_id int ,
+collection_id int,
+foreign key (track_id) references track(track_id),  
+foreign key (collection_id) references collection(collection_id));
 
 
